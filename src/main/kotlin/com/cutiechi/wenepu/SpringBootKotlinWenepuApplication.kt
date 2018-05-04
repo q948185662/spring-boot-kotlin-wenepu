29579827a6c79c44b741c314b8de8a76adc9547a
superchijinpeng <superchijinpeng@gmail.com>
2018-05-04 16:11:39 +0800

    Initial commit

---
+++ src/main/kotlin/com/cutiechi/wenepu/SpringBootKotlinWenepuApplication.kt
@@ -0,0 +1,11 @@
+package com.cutiechi.wenepu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinWenepuApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootKotlinWenepuApplication>(*args)
+}
