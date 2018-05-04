66d945ca94aafc9797460d7bf4381857aae40a64
superchijinpeng <superchijinpeng@gmail.com>
2018-05-04 23:04:28 +0800

    feat(api): add get app token api

--- src/main/kotlin/com/cutiechi/wenepu/controller/AuthenticationController.kt
+++ src/main/kotlin/com/cutiechi/wenepu/controller/AuthenticationController.kt
@@ -1,6 +1,12 @@
 package com.cutiechi.wenepu.controller
 
+import com.cutiechi.wenepu.exception.AuthenticationErrorException
+import com.cutiechi.wenepu.exception.ServerErrorException
+import com.cutiechi.wenepu.service.AuthenticationService
+
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -9,4 +15,34 @@ import org.springframework.web.bind.annotation.RestController
 @CrossOrigin
 class AuthenticationController {
 
+    @Autowired
+    private lateinit var service: AuthenticationService
+
+    @PostMapping("/app-token", produces = ["application/json;charset=UTF-8"])
+    fun getAppToken(userName: String, password: String): String {
+        return try {
+            val appToken = service.getAppToken(userName, password)
+            """
+            {
+                "code": 200,
+                "message": "获取 app token 成功！",
+                "appToken": "$appToken"
+            }
+            """.trimIndent()
+        } catch (exception: AuthenticationErrorException) {
+            """
+            {
+                "code": 401,
+                "message": "用户名或密码错误，获取 app token 失败！"
+            }
+            """.trimIndent()
+        } catch (exception: ServerErrorException) {
+            """
+            {
+                "code": 500,
+                "message": "服务器错误，获取 app token 失败！"
+            }
+            """.trimIndent()
+        }
+    }
 }
