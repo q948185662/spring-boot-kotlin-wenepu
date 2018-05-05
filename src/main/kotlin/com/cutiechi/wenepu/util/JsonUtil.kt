da744dfd639ac40df217aca2f00d3724a0c330fd
superchijinpeng <superchijinpeng@gmail.com>
2018-05-05 09:23:40 +0800

    feat(util): add any to json method

--- src/main/kotlin/com/cutiechi/wenepu/util/JsonUtil.kt
+++ src/main/kotlin/com/cutiechi/wenepu/util/JsonUtil.kt
@@ -1,8 +1,12 @@
 package com.cutiechi.wenepu.util
 
+import com.google.gson.Gson
 import com.google.gson.JsonParser
 
 object JsonUtil {
+    private val gson = Gson()
 
     fun getValue(json: String?, key: String): String = JsonParser().parse(json).asJsonObject.get(key).asString
+
+    fun toJson(src: Any): String = gson.toJson(src)
 }
