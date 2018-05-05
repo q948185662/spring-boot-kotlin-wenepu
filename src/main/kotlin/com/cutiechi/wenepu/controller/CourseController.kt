81dd74db1b6a90c1e2531bda714127a888425ba2
superchijinpeng <superchijinpeng@gmail.com>
2018-05-05 10:18:33 +0800

    feat(controller): add course controller

---
+++ src/main/kotlin/com/cutiechi/wenepu/controller/CourseController.kt
@@ -0,0 +1,13 @@
+package com.cutiechi.wenepu.controller
+
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@CrossOrigin
+@RequestMapping("/courses")
+class CourseController {
+
+}
