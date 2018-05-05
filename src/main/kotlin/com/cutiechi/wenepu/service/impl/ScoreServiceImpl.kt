abcaa242e5d9a53a6182c67682cf593fbd05c542
superchijinpeng <superchijinpeng@gmail.com>
2018-05-05 09:10:20 +0800

    feat(service): add score service interface and impl

---
+++ src/main/kotlin/com/cutiechi/wenepu/service/impl/ScoreServiceImpl.kt
@@ -0,0 +1,10 @@
+package com.cutiechi.wenepu.service.impl
+
+import com.cutiechi.wenepu.service.ScoreService
+
+import org.springframework.stereotype.Service
+
+@Service
+class ScoreServiceImpl : ScoreService {
+
+}
