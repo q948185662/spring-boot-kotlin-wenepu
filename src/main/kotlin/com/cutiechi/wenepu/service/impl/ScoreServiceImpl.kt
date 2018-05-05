57834cecd6380657a82c33ae73b437ee60619678
superchijinpeng <superchijinpeng@gmail.com>
2018-05-05 09:39:02 +0800

    feat(service): add get semester list method impl

--- src/main/kotlin/com/cutiechi/wenepu/service/impl/ScoreServiceImpl.kt
+++ src/main/kotlin/com/cutiechi/wenepu/service/impl/ScoreServiceImpl.kt
@@ -1,10 +1,41 @@
 package com.cutiechi.wenepu.service.impl
 
+import com.cutiechi.wenepu.exception.ServerErrorException
+import com.cutiechi.wenepu.exception.TokenErrorException
 import com.cutiechi.wenepu.service.ScoreService
+import com.cutiechi.wenepu.util.JsonUtil
+import org.jsoup.Jsoup
 
 import org.springframework.stereotype.Service
 
 @Service
 class ScoreServiceImpl : ScoreService {
 
+    override fun getSemesterList(webToken: String): String {
+        val connection = Jsoup.connect("http://jwgl.nepu.edu.cn/tkglAction.do")
+                .data("method", "kbxxXs")
+                .cookie("JSESSIONID", webToken)
+                .timeout(6000)
+        return try {
+            val document = connection.get()
+            val title = document.title()
+            when {
+                title != "出错页面" -> {
+                    val elements = document.getElementById("xnxqh").select("option")
+                    val semesterList = ArrayList<String>()
+                    for (element in elements) {
+                        val semester = element.html()
+                        when (semester) {
+                            "---请选择---" -> semesterList.add("全部学期")
+                            else -> semesterList.add(semester)
+                        }
+                    }
+                    JsonUtil.toJson(semesterList)
+                }
+                else -> throw TokenErrorException()
+            }
+        } catch (exception: Exception) {
+            throw ServerErrorException()
+        }
+    }
 }
