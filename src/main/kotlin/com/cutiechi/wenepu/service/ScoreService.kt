3f2547db42e16ec74b1f8d37978cc8bcd31704ce
superchijinpeng <superchijinpeng@gmail.com>
2018-05-05 10:51:23 +0800

    feat(service): add get score detail method interface

--- src/main/kotlin/com/cutiechi/wenepu/service/ScoreService.kt
+++ src/main/kotlin/com/cutiechi/wenepu/service/ScoreService.kt
@@ -4,4 +4,6 @@ interface ScoreService {
     fun getSemesterList(webToken: String): String
 
     fun getScores(semester: String, webToken: String): String
+
+    fun getScoreDetail(scoreDetailUrl: String, webToken: String): String
 }
