1ca2a71da14211ca45decd04c5551b3980cdcd03
superchijinpeng <superchijinpeng@gmail.com>
2018-05-05 10:17:11 +0800

    feat(model): add course item model

---
+++ src/main/kotlin/com/cutiechi/wenepu/model/CourseItem.kt
@@ -0,0 +1,7 @@
+package com.cutiechi.wenepu.model
+
+data class CourseItem(var courseName: String,
+                      var courseClass: String,
+                      var courseTeacher: String,
+                      var courseWeekly: String,
+                      var courseRoom: String)
