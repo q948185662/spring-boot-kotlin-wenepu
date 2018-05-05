2ba61ce298e92dd6646126a7c312ed08627ab628
superchijinpeng <superchijinpeng@gmail.com>
2018-05-05 10:40:45 +0800

    feat(service): add get scores method impl

--- src/main/kotlin/com/cutiechi/wenepu/service/impl/ScoreServiceImpl.kt
+++ src/main/kotlin/com/cutiechi/wenepu/service/impl/ScoreServiceImpl.kt
@@ -2,6 +2,7 @@ package com.cutiechi.wenepu.service.impl
 
 import com.cutiechi.wenepu.exception.ServerErrorException
 import com.cutiechi.wenepu.exception.TokenErrorException
+import com.cutiechi.wenepu.model.ScoreItem
 import com.cutiechi.wenepu.service.ScoreService
 import com.cutiechi.wenepu.util.JsonUtil
 import org.jsoup.Jsoup
@@ -31,6 +32,68 @@ class ScoreServiceImpl : ScoreService {
                         }
                     }
                     JsonUtil.toJson(semesterList)
+                }
+                else -> throw TokenErrorException()
+            }
+        } catch (exception: Exception) {
+            throw ServerErrorException()
+        }
+    }
+
+    override fun getScores(semester: String, webToken: String): String {
+        val connection = Jsoup.connect("http://jwgl.nepu.edu.cn/xszqcjglAction.do?method=queryxscj")
+                .data("kksj", semester)
+                .cookie("JSESSIONID", webToken)
+                .timeout(6000)
+        return try {
+            val document = connection.post()
+            val title = document.title()
+            when {
+                title != "出错页面" -> {
+                    val credit = document.getElementById("tblBm").select("td")[0].select("span")[1].html()
+                    var gradePointAverage = document.getElementById("tblBm").select("td")[0].select("span")[3].html()
+                    gradePointAverage = gradePointAverage.substring(0, gradePointAverage.length - 1)
+                    val elements = document.getElementById("mxh").getAllElements().select("tr")
+                    val scoreList = ArrayList<ScoreItem>()
+                    for (element in elements) {
+                        var courseId = element.select("td")[0].html()
+                        courseId = courseId.substring(courseId.indexOf(";") + 1)
+                        val courseSemester = element.select("td")[3].html()
+                        val courseName = element.select("td")[4].html()
+                        val courseScore = element.select("td")[5].select("a")[0].html()
+                        var scoreDetailUrl = element.select("td")[5].select("a")[0].attr("onclick")
+                        scoreDetailUrl = scoreDetailUrl.substring(scoreDetailUrl.indexOf("/"), scoreDetailUrl.lastIndexOf("'"))
+                        var scoreSign = element.select("td")[6].html()
+                        scoreSign = scoreSign.substring(scoreSign.indexOf(";") + 1)
+                        val courseNature = element.select("td")[7].html()
+                        val courseType = element.select("td")[8].html()
+                        val courseHour = element.select("td")[9].html()
+                        val courseCredit = element.select("td")[10].html()
+                        val examNature = element.select("td")[11].html()
+                        var supplementSemester = element.select("td")[12].html()
+                        supplementSemester = supplementSemester.substring(supplementSemester.indexOf(";") + 1)
+                        scoreList.add(ScoreItem(
+                                courseId,
+                                courseSemester,
+                                courseName,
+                                courseScore,
+                                scoreDetailUrl,
+                                scoreSign,
+                                courseNature,
+                                courseType,
+                                courseHour,
+                                courseCredit,
+                                examNature,
+                                supplementSemester
+                        ))
+                    }
+                    """
+                    {
+                        "credit": "$credit",
+                        "gradePointAverage": "$gradePointAverage",
+                        "scoreList": ${JsonUtil.toJson(scoreList)}
+                    }
+                    """.trimIndent()
                 }
                 else -> throw TokenErrorException()
             }
