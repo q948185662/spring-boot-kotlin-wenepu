be26b209f00bc0f35a9edd7535564c93f903c2e7
superchijinpeng <superchijinpeng@gmail.com>
2018-05-05 23:53:43 +0800

    feat(service): add classroom service interface and impl

---
+++ src/main/kotlin/com/cutiechi/wenepu/service/impl/ClassroomServiceImpl.kt
@@ -0,0 +1,11 @@
+package com.cutiechi.wenepu.service.impl
+
+import com.cutiechi.wenepu.service.ClassroomService
+
+import org.springframework.stereotype.Service
+
+
+@Service
+class ClassroomServiceImpl : ClassroomService {
+
+}
