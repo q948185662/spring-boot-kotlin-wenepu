e654db1f32d0e5a9bd0cd45fba19e87b9edc86f0
superchijinpeng <superchijinpeng@gmail.com>
2018-05-05 09:40:18 +0800

    feat(controller): add score controller

---
+++ src/main/kotlin/com/cutiechi/wenepu/controller/ScoreController.kt
@@ -0,0 +1,10 @@
+package com.cutiechi.wenepu.controller
+
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@CrossOrigin
+class ScoreController {
+
+}
