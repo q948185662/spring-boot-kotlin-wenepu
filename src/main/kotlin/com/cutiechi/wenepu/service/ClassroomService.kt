120b9c4947ef1d1e2b0e1a3a879a0e9c18a185ac
superchijinpeng <superchijinpeng@gmail.com>
2018-05-05 23:54:29 +0800

    feat(service): add get free classroom list method interface

--- src/main/kotlin/com/cutiechi/wenepu/service/ClassroomService.kt
+++ src/main/kotlin/com/cutiechi/wenepu/service/ClassroomService.kt
@@ -1,5 +1,11 @@
 package com.cutiechi.wenepu.service
 
 interface ClassroomService {
-
+    fun getFreeClassroomList(
+            freeDate: String,
+            freeTime: String,
+            teachingBuildingId: String,
+            seatNumber: String,
+            appToken: String
+    ): String
 }
