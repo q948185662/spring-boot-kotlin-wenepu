ee26e36db7b820cec79b5e3447ec9b5371d93672
superchijinpeng <superchijinpeng@gmail.com>
2018-05-12 13:00:36 +0800

    feat(service): add time service impl

---
+++ src/main/kotlin/com/cutiechi/wenepu/service/impl/TimeServiceImpl.kt
@@ -0,0 +1,9 @@
+package com.cutiechi.wenepu.service.impl
+
+import com.cutiechi.wenepu.service.TimeService
+import org.springframework.stereotype.Service
+
+@Service
+class TimeServiceImpl : TimeService {
+
+}
