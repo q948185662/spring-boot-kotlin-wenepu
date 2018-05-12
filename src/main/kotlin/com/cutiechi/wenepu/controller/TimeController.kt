08799eefc65411d290fb4ff37b6df1cf8e13fdd1
superchijinpeng <superchijinpeng@gmail.com>
2018-05-12 13:37:18 +0800

    feat(controller): add time controller

---
+++ src/main/kotlin/com/cutiechi/wenepu/controller/TimeController.kt
@@ -0,0 +1,11 @@
+package com.cutiechi.wenepu.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RequestMapping("/time")
+@RestController
+class TimeController {
+
+}
