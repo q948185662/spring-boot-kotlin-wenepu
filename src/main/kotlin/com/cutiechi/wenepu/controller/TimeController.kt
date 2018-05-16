5bd589fefb8cbb9213f778035650490f054f70f1
superchijinpeng <superchijinpeng@gmail.com>
2018-05-16 13:45:25 +0800

    refactor(api): rename get current time method to get current week

--- src/main/kotlin/com/cutiechi/wenepu/controller/TimeController.kt
+++ src/main/kotlin/com/cutiechi/wenepu/controller/TimeController.kt
@@ -18,30 +18,30 @@ class TimeController {
     private lateinit var service: TimeService
 
     @PostMapping("/current", produces = ["application/json;charset=UTF-8"])
-    fun getCurrentTime(
+    fun getCurrentWeek(
             currentDate: String,
             @RequestHeader("appToken") appToken: String
     ) = try {
-        val currentTime = service.getCurrentTime(currentDate, appToken)
+        val currentWeek = service.getCurrentWeek(currentDate, appToken)
         """
         {
             "code": 200,
-            "message": "获取当前时间成功！",
-            "currentTime": $currentTime
+            "message": "获取当前周成功！",
+            "currentTime": $currentWeek
         }
         """.trimIndent()
     } catch (exception: TokenErrorException) {
         """
             {
                 "code": 403,
-                "message": "app token 错误，获取当前时间失败！"
+                "message": "app token 错误，获取当前周失败！"
             }
             """.trimIndent()
     } catch (exception: ServerErrorException) {
         """
             {
                 "code": 500,
-                "message": "服务器错误，获取当前时间失败！"
+                "message": "服务器错误，获取当前周失败！"
             }
             """.trimIndent()
     }
